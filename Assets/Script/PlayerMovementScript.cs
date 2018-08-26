f5aa6cfd2e42b7dd25d36ab4d54400c93b4969b0
TropicMango <charliean19@gmail.com>
2018-08-25 20:13:50 -0700

    initial Commit

---
+++ Assets/Script/PlayerMovementScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+    public float movementSpeed;
+    public Rigidbody2D Rb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey(KeyCode.W)){
+            Rb.AddForce(new Vector2(0, movementSpeed));
+        } else if (Input.GetKey(KeyCode.S)){
+            Rb.AddForce(new Vector2(0, -1*movementSpeed));
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            Rb.AddForce(new Vector2(-1*movementSpeed, 0));
+        } else if (Input.GetKey(KeyCode.D)) {
+            Rb.AddForce(new Vector2(movementSpeed, 0));
+        }
+    }
+}
---
+++ Assets/Script/ShieldMovementScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldMovementScript : MonoBehaviour {
+
+    public float RotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.Rotate(new Vector3(0, 0, RotationSpeed));
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.Rotate(new Vector3(0, 0, -RotationSpeed));
+            
+        }
+    }
+}
