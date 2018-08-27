f1a0a4d7f15cb19a0189147eb544d7e6e0d09762
TropicMango <charliean19@gmail.com>
2018-08-27 14:49:09 -0700

    map updates

---
+++ Assets/Script/RoomGenerationScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomGenerationScript : MonoBehaviour {
+
+    // all of thse stand for the side you can exit
+    public GameObject[] Room; 
+    public GameObject[] Door; // Order: up, down, left, right
+    public GameObject[] Hall; // Order: Horizontal, Vertical
+    
+
+    // Use this for initialization
+    void Start () {
+        // Instantiate(Room[0], new Vector3(0, 0, 0), Quaternion.Euler(0,0,0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
