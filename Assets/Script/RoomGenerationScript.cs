02df156fafc1d7516b5a044cbf11b1319b64908e
TropicMango <charliean19@gmail.com>
2018-08-29 00:52:30 -0700

    map rework

--- Assets/Script/RoomGenerationScript.cs
+++ Assets/Script/RoomGenerationScript.cs
@@ -12,8 +12,15 @@ public class RoomGenerationScript : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        // Instantiate(Room[0], new Vector3(0, 0, 0), Quaternion.Euler(0,0,0));
-	}
+        Instantiate(Room[0], new Vector3(0, 0, 100), Quaternion.Euler(0,0,0));
+        Instantiate(Room[0], new Vector3(0, 25, 100), Quaternion.Euler(0, 0, 0));
+        Instantiate(Room[0], new Vector3(25, 0, 100), Quaternion.Euler(0, 0, 0));
+        Instantiate(Room[0], new Vector3(0, -25, 100), Quaternion.Euler(0, 0, 0));
+
+        Instantiate(Hall[0], new Vector3(0, 12.5f, 120), Quaternion.Euler(0, 0, 90));
+        Instantiate(Hall[0], new Vector3(0, -12.5f, 120), Quaternion.Euler(0, 0, 90));
+        Instantiate(Hall[0], new Vector3(12.5f, 0, 120), Quaternion.Euler(0, 0, 0));
+    }
 	
 	// Update is called once per frame
 	void Update () {
